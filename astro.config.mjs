82cc93563501f17b00f1245b94b512fafa26b470
gmtstephane <gmtstephane@gmail.com>
2024-03-17 19:56:52 +0100

    up

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,14 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
-
 import sentry from "@sentry/astro";
-import spotlightjs from "@spotlightjs/astro";
+
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: node({
-    mode: 'standalone'
+    mode: "standalone"
   }),
   integrations: [svelte(), tailwind(), sentry()]
 });
